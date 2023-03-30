5bd11a749a7e1b200047c73bafe3b09c5d6e4433
Mario Munoz <mikomunoz459@gmail.com>
2023-03-30 14:58:00 -0700

    feature(fetch): GET request work

--- flask-server/server.py
+++ flask-server/server.py
@@ -33,7 +33,7 @@ def CreateAccount():
 
 @app.route('/login', methods=['GET', 'POST'])
 def Login():
-    if request.method == 'POST':
+    if request.method == 'GET':
         #Fetch form data
         userDetails = request.json
         print("userDetails:", str(userDetails))
@@ -41,11 +41,20 @@ def Login():
         userPasswordToHash = userDetails['passwordInput']
         userPassword = HandlePasswordHashing(userPasswordToHash)
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO userlogindata(userName, userPassword) VALUES(%s, %s)', (userName, userPassword))
+        cur.execute('SELECT * FROM userlogindata WHERE userName = %s AND userPassword = %s', (userName, userPassword,))
         mysql.connection.commit()
         cur.close()
-        return '200'
-    return 'hi'
+        account = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
 
 def HandlePasswordHashing(passwordToHash):
     hashPassword = hashlib.md5(passwordToHash.encode())
